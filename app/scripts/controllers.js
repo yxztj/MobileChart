ffed1926cc7c4e0e6035bffaf265c7d9b96af243
Jason <Jason@192.168.1.101>
2014-05-11 19:50:36 +0800

    initial commit

---
+++ app/scripts/controllers.js
@@ -0,0 +1,18 @@
+'use strict';
+angular.module('Yoionic.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+
+  $scope.names = ['Igor Minar', 'Brad Green', 'Dave Geddes', 'Naomi Black', 'Greg Weber', 'Dean Sofer', 'Wes Alvaro', 'John Scott', 'Daniel Nadasi'];
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.friend = Friends.get($stateParams.friendId);
+})
+
+.controller('AccountCtrl', function($scope) {
+});
