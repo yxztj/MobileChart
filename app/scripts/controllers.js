2b57676b88f62cb5693209ad7ed20801608af819
Jason <Jason@192.168.1.101>
2014-05-12 23:27:26 +0800

    mobile pin

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,10 +1,49 @@
 'use strict';
 angular.module('Yoionic.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('DashCtrl', function($scope, $ionicModal) {
 
   $scope.names = ['Igor Minar', 'Brad Green', 'Dave Geddes', 'Naomi Black', 'Greg Weber', 'Dean Sofer', 'Wes Alvaro', 'John Scott', 'Daniel Nadasi'];
+  
+  $ionicModal.fromTemplateUrl('templates/my-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up',
+    focusFirstInput: true,
+    focusFirstDelay: 200,
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+    $scope.password="";
+
+    console.log($scope.modal);
+    
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  //Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+/*
+  $scope.$watch('password', function(newVal, oldVal){
+  	
+  });*/
+
+
+  $scope.checkPW = function () {
+  	console.log("checking password", $scope.password);
+    if($scope.password=="abcd") {
+    	console.log("mactched!");
+    	$scope.closeModal();
+    } 
+  }
+
 })
+
 
 .controller('FriendsCtrl', function($scope, Friends, $timeout) {
 	$timeout(function () {
